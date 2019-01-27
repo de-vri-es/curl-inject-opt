c534a2f73f9c2395ecdb0f315a2cdbdfe0566542
Maarten de Vries <maarten@de-vri.es>
2019-01-27 16:04:26 +0100

    Don't use option name for option value name with clap.

--- application/src/bin/curl-inject-opt.rs
+++ application/src/bin/curl-inject-opt.rs
@@ -19,6 +19,7 @@ fn build_clap<'a, 'b>() -> clap::App<'a, 'b> {
 		app = app.arg(clap::Arg::with_name(option.name)
 			.long(option.name)
 			.takes_value(true)
+			.value_name("VAL")
 			.help(option.help)
 		);
 	}
